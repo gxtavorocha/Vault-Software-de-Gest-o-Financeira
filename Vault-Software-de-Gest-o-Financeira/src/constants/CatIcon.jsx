338e34d2fce6c0813e64a2282e4df28fecb31444
Gustavo Rocha <rochagu.oliveira@gmail.com>
2026-03-16 21:48:58 -0300

    resolvendo erro de caseSensitive

--- Vault-Software-de-Gest-o-Financeira/src/constants/CatIcon.jsx
+++ Vault-Software-de-Gest-o-Financeira/src/constants/CatIcon.jsx
@@ -1,7 +1,6 @@
 import { ICON_MAP } from "./index";
 
-// Renderiza o ícone de uma categoria a partir da chave string salva no localStorage.
-// Uso: <CatIcon name={cat.icon} size={18} color={cat.color} />
+
 export function CatIcon({ name, size = 18, color, style = {} }) {
   const Component = ICON_MAP[name];
   if (!Component)
